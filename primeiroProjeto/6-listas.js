4fc30357d67a39dc4e44981992df4d6ad532840c
yelahnibow <151558408+yelahnibow@users.noreply.github.com>
2024-09-19 08:41:17 -0300

    Add files via upload

--- primeiroProjeto/6-listas.js
+++ primeiroProjeto/6-listas.js
@@ -1,1 +1,19 @@
 console.log('Trabalhando com listas')
+// const PR = 'Paraná'
+// const SP = 'São Paulo';
+// const RJ = 'Rio de Janeiro';
+
+const listaDestinos = new Array(
+    'Paraná',
+    'São Paulo',
+    'Rio de Janeiro'
+
+
+);
+
+listaDestinos.push('Pará') //adicionar um item na lista
+console.log("Destinos possíveis:");
+// console.log(PR, SP, RJ )
+console.log(listaDestinos);
+
+listaDestinos = 2;
